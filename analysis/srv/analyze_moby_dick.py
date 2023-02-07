08247595a7fa8c801308072d1c0604bd265c737b
Landon Miller <millerlm012@gmail.com>
2023-02-06 18:49:33 -0600

    Added further checks to ensure that we're only counting words from book and not the pre-writing / end-writing
    - count was containing words like "project" and "gutenberg" which have nothing to do with Moby Dick
    	- resolved via ensuring that we start and stop counting after the pre-writing title and before the end-writing title

--- analysis/srv/analyze_moby_dick.py
+++ analysis/srv/analyze_moby_dick.py
@@ -16,7 +16,7 @@ def clean_line(line):
     """
     clean_line() is utilized for replacing hyphens, the possesive "'s", and the listed punctuation below with a space in the specified {line}.
     """
-    line = line.replace('-', ' ').replace('—', ' ').replace('’s', ' ')
+    line = line.replace('-', ' ').replace('—', ' ').replace('’s', '')
     for punc in """!@#$%^&*()=+_`~[]\|;:'",./<>?“”""":
         line = line.replace(punc, '')
     return line
@@ -26,16 +26,24 @@ def count_top_100_frequent_words():
     stop_words = load_file('stop-words.txt')
     moby_text = load_file('mobydick.txt')
 
+    start = False
     for line in moby_text:
+        if line == 'End of Project Gutenberg’s Moby Dick; or The Whale, by Herman Melville':
+            break
+
         line = clean_line(line)
         line = line.strip().lower()
 
-        for word in line.split(' '):
-            if word not in stop_words and word != '':
-                try:
-                    occurrences[word] += 1
-                except KeyError:
-                    occurrences[word] = 1
+        if start:
+            for word in line.split(' '):
+                if word not in stop_words and word != '':
+                    try:
+                        occurrences[word] += 1
+                    except KeyError:
+                        occurrences[word] = 1
+
+        if line == 'start of this project gutenberg ebook moby dick or the whale':
+            start = True
     
     occurrences_sorted = []
     for key, value in occurrences.items():
