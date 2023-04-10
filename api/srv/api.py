24dbb8feb0567a106520e5824fc6b1ae3804bfcd
Landon Miller <millerlm012@gmail.com>
2023-04-10 11:25:00 -0500

    Finalized go merge to main

--- api/srv/api.py
+++ api/srv/api.py
@@ -6,16 +6,28 @@ from bottle import route, run, response, HTTPResponse
 import json
 import os
 
+IMPLEMENTATION = os.environ['IMPLEMENTATION']
+FILE_PATH = f'/{IMPLEMENTATION}-files'
+
 @route('/api/top_100')
 def get_top_100():
     # if our file does not exist, return HTTP 500. This will happen if the analysis container was not ran and the results weren't written to it's appropriate location
-    top_100_path = '/files/top_100.txt'
+    top_100_path = f'{FILE_PATH}/top_100.txt'
     if not os.path.exists(top_100_path):
         return HTTPResponse(status=500, body=json.dumps([{'Error': 'The top 100 file doesn\'t exist yet... Did the analysis container run appropriately?'}])) 
 
     # read our json stored top 100 and return a successful response
     with open(top_100_path) as f:
         data = f.read()
+
+    if IMPLEMENTATION == 'python':
+        new_data = []
+        for row in json.loads(data):
+            obj = {}
+            obj['Word'] = row[0]
+            obj['Count'] = row[1]
+            new_data.append(obj)
+        data = json.dumps(new_data)
 
     response.content_type = 'application/json'
     return HTTPResponse(status=200, body=data)
